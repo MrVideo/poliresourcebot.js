ee1a2c058af1def0355e4292c3ab475950c49043
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-10-04 17:43:27 +0200

    Remove unused parameter

--- commands/search.js
+++ commands/search.js
@@ -48,7 +48,7 @@ module.exports = {
             // Query the database for the resources matching this
             // A promise is used once again to make sure that the query executes before sending the result to the user
             const results = await new Promise((resolve, reject) => {
-                con.query("SELECT R.Name, R.Link, A.FirstName, A.LastName, A.DiscordUsername FROM Resources R, Authors A WHERE R.Name LIKE ? AND R.AuthorID = A.ID;", [query], function (err, result, fields) {
+                con.query("SELECT R.Name, R.Link, A.FirstName, A.LastName, A.DiscordUsername FROM Resources R, Authors A WHERE R.Name LIKE ? AND R.AuthorID = A.ID;", [query], function (err, result) {
                     if (err) {
                         reject(err);
                     } else {

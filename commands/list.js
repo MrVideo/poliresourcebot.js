6b11d6513e995f051c5b345e926af071cfcfe817
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2023-10-04 18:04:31 +0200

    Add defer reply and db connection initialisation

--- commands/list.js
+++ commands/list.js
@@ -6,7 +6,19 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('list')
         .setDescription('Elenca le risorse disponibili.'),
+
     async execute(interaction) {
+        // Defer reply
+        await interaction.deferReply();
+
+        // Initialise connection
+        const con = mysql.createConnection({
+            host: "host",
+            user: "user",
+            password: "password",
+            database: "database"
+        });
+
         // Embed initialization
         let listEmbed = new EmbedBuilder().setTitle('Lista delle risorse');
 

e7a3651cd6a6fb77a35ae4ef5b9da7327c7a8996
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2024-01-24 11:19:05 +0100

    Migrate to SQLite

--- commands/search.js
+++ commands/search.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-// Initialise MySQL driver
-const mysql = require("mysql");
+// Initialise SQLite driver
+const sqlite = require('sqlite3');
+const { dbName } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -15,81 +16,45 @@ module.exports = {
         // Defer reply
         await interaction.deferReply();
 
-        // Initialise DB connection
-        const con = mysql.createConnection({
-            host: "host",
-            user: "user",
-            password: "password",
-            database: "database"
-        });
+		try {
+			const db = new sqlite.Database(dbName);
 
-        try {
-            // Connect to the database
-            // The promise is used to make sure that the connection is successful before continuing with the query
-            await new Promise((resolve, reject) => {
-                con.connect(function (err) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        console.log("Database connection successful");
-                        resolve();
-                    }
-                });
-            });
+			// Initialize embed
+			let queryResponseEmbed = new EmbedBuilder()
+				.setTitle('Risultati di ricerca')
+				.setDescription('Ecco cosa ho trovato!');
 
-            // Initialize embed
-            let queryResponseEmbed = new EmbedBuilder()
-                .setTitle('Risultati di ricerca')
-                .setDescription('Ecco cosa ho trovato!');
+			// Save search query and format it
+			const query = "%" + interaction.options.getString('query') + "%";
+			const queryStatement = db.prepare('SELECT Name, URL, Username FROM Resources WHERE Name LIKE ?');
 
-            // Save search query and format it
-            const query = "%" + interaction.options.getString('query') + "%";
+			const rows = await new Promise((resolve, reject) => {
+				queryStatement.all([query], (err, rows) => {
+					queryStatement.finalize();
 
-            // Query the database for the resources matching this
-            // A promise is used once again to make sure that the query executes before sending the result to the user
-            const results = await new Promise((resolve, reject) => {
-                con.query("SELECT R.Name, R.Link, A.FirstName, A.LastName, A.DiscordUsername FROM Resources R, Authors A WHERE R.Name LIKE ? AND R.AuthorID = A.ID;", [query], function (err, result) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(result);
-                    }
-                });
-            });
+					if (err)
+						reject(err);
+					else resolve(rows);
+				});
+			});
 
-            // This variable makes sure not to send the embed if no results were found
-            let done = false;
+			if (rows.length !== 0) {
+				rows.forEach((row) => {
+					queryResponseEmbed.addFields({
+						name: `${row.Name} by @${row.Username}`,
+						value: row.URL
+					})
+				});
+				
+				await interaction.editReply({ embeds: [queryResponseEmbed] });
+			} else {
+				await interaction.editReply('Mi dispiace, non ho trovato risultati che corrispondano alla tua ricerca.');
+			}
 
-            if (results.length === 0) {
-                // This is an error message that tells the user no item corresponds to their query
-                await interaction.editReply("Mi dispiace, ma non ho trovato nulla che corrisponda ai tuoi criteri di ricerca.");
-                done = true;
-            } else {
-                // If the query returned some items, build the embed with their name and link
-                for (const row of results) {
-                    if(row.FirstName === null || row.LastName === null) {
-                        queryResponseEmbed.addFields({
-                            name: `${row.Name} by ${row.DiscordUsername}`,
-                            value: row.Link
-                        });
-                    } else {
-                        queryResponseEmbed.addFields({
-                            name: `${row.Name} by ${row.FirstName} ${row.LastName}`,
-                            value: row.Link
-                        });
-                    }
-                }
-            }
-
-            // Return search result
-            if(!done)
-                await interaction.editReply({ embeds: [queryResponseEmbed] });
-        } catch (err) {
-            console.error('Error:', err);
-            await interaction.followUp('Si è verificato un errore durante la ricerca.');
-        } finally {
-            // Close the database connection
-            con.end();
-        }
+			db.close();
+		} catch (err) {
+			await interaction.followUp('Si è verificato un errore.');
+			console.log('Error: ', err);
+		}
     },
 };

31da41cd154bd9db08bb20684d499d5fd2dbb145
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2024-01-24 11:17:37 +0100

    Change data in about embed

--- commands/about.js
+++ commands/about.js
@@ -8,7 +8,7 @@ module.exports = {
         // Embed declaration
         const aboutEmbed = {
             title: 'PoliResourceBot',
-            description: 'Bot creato da Mario Merlo usando la libreria discord.js ~~e tanta documentazione~~',
+            description: 'Bot creato da @xmrvideo usando la libreria discord.js ~~e tanta documentazione~~',
             thumbnail: {
                 url: 'https://i.ibb.co/BLTq2mH/icon.png'
             },
@@ -18,17 +18,9 @@ module.exports = {
                     value: '[Clicca qui](https://github.com/MrVideo/poliresourcebot.js)'
                 },
                 {
-                    name: 'Documentazione Bot',
-                    value: 'In arrivo'
-                },
-                {
                     name: 'Il mio sito',
                     value: '[Clicca qui](https://mariomerlo.me)'
                 },
-                {
-                    name: 'Il mio canale',
-                    value: '[YouTube](https://www.youtube.com/watch?v=0ASiDR5Yj0k)'
-                }
             ]
         }
 
